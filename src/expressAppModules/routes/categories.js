de43754b4b756d8eb36cb5d3349ca89accc4d7e0
humbertotm <humberto.htm@gmail.com>
2017-05-31 20:40:46 -0500

    Added route and controller to add categories.
    Added route and controller to add categories (could do this through
    mongo shell).

--- src/expressAppModules/routes/categories.js
+++ src/expressAppModules/routes/categories.js
@@ -11,4 +11,9 @@ categoryRoutes.get('/:categoryId', categories_controller.get_posts);
 // Gets more posts from specified category starting at provided maxId.
 categoryRoutes.get('/:categoryId/:maxId', categories_controller.get_more_posts);
 
+// Create a category.
+// Did this because it was the quickest way to create the categories,
+// given the fact that I do not know how to do it through the mono shell, yet.
+categoryRoutes.post('/', categories_controller.create_category);
+
 module.exports = categoryRoutes;
