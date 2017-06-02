9e10ef1a22b08a02af1a22959e9d0100a709c324
humbertotm <humberto.htm@gmail.com>
2017-06-02 02:27:49 -0500

    security change
    security change.

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://humbertotm:Ballade#1@ds139791.mlab.com:39791/vandalore-dev'
+    'url': 'mongodb://<user>:<password>@ds139791.mlab.com:39791/vandalore-dev'
 };
